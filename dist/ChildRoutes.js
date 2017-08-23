c950fd61dbbabd4bbb96c1a39a10432376b58ffc
Christian Spilhere <cspilhere@gmail.com>
2017-08-22 21:40:24 -0300

    Initial commit

--- dist/ChildRoutes.js
+++ dist/ChildRoutes.js
@@ -1,54 +1,28 @@
-(function (global, factory) {
-  if (typeof define === "function" && define.amd) {
-    define(['exports', 'babel-runtime/helpers/extends', 'react', 'react-router-dom'], factory);
-  } else if (typeof exports !== "undefined") {
-    factory(exports, require('babel-runtime/helpers/extends'), require('react'), require('react-router-dom'));
-  } else {
-    var mod = {
-      exports: {}
-    };
-    factory(mod.exports, global._extends, global.react, global.reactRouterDom);
-    global.ChildRoutes = mod.exports;
-  }
-})(this, function (exports, _extends2, _react, _reactRouterDom) {
-  'use strict';
+import _extends from 'babel-runtime/helpers/extends';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
 
-  Object.defineProperty(exports, "__esModule", {
-    value: true
-  });
-
-  var _extends3 = _interopRequireDefault(_extends2);
-
-  var _react2 = _interopRequireDefault(_react);
-
-  function _interopRequireDefault(obj) {
-    return obj && obj.__esModule ? obj : {
-      default: obj
-    };
-  }
-
-  var ChildRoutes = function ChildRoutes(props) {
-    var routes = (props.routes || []).map(function (route, key) {
-      return _react2.default.createElement(_reactRouterDom.Route, {
-        key: key,
-        location: props.location,
-        exact: route.exact,
-        path: route.path,
-        render: function render(routeProps) {
-          return _react2.default.createElement(route.component, (0, _extends3.default)({}, routeProps, route));
-        }
-      });
+var ChildRoutes = function ChildRoutes(props) {
+  var routes = (props.routes || []).map(function (route, key) {
+    return React.createElement(Route, {
+      key: key,
+      location: props.location,
+      exact: route.exact,
+      path: route.path,
+      render: function render(routeProps) {
+        return React.createElement(route.component, _extends({}, routeProps, route));
+      }
     });
-    return _react2.default.createElement(
-      'div',
-      { className: 'routes-container' },
-      _react2.default.createElement(
-        _reactRouterDom.Switch,
-        { location: props.location },
-        routes
-      )
-    );
-  };
+  });
+  return React.createElement(
+    'div',
+    { className: 'routes-container' },
+    React.createElement(
+      Switch,
+      { location: props.location },
+      routes
+    )
+  );
+};
 
-  exports.default = ChildRoutes;
-});
+export default ChildRoutes;
--- dist/ScrollToTop.js
+++ dist/ScrollToTop.js
@@ -1,66 +1,30 @@
-(function (global, factory) {
-  if (typeof define === "function" && define.amd) {
-    define(['exports', 'babel-runtime/core-js/object/get-prototype-of', 'babel-runtime/helpers/classCallCheck', 'babel-runtime/helpers/createClass', 'babel-runtime/helpers/possibleConstructorReturn', 'babel-runtime/helpers/inherits', 'react'], factory);
-  } else if (typeof exports !== "undefined") {
-    factory(exports, require('babel-runtime/core-js/object/get-prototype-of'), require('babel-runtime/helpers/classCallCheck'), require('babel-runtime/helpers/createClass'), require('babel-runtime/helpers/possibleConstructorReturn'), require('babel-runtime/helpers/inherits'), require('react'));
-  } else {
-    var mod = {
-      exports: {}
-    };
-    factory(mod.exports, global.getPrototypeOf, global.classCallCheck, global.createClass, global.possibleConstructorReturn, global.inherits, global.react);
-    global.ScrollToTop = mod.exports;
-  }
-})(this, function (exports, _getPrototypeOf, _classCallCheck2, _createClass2, _possibleConstructorReturn2, _inherits2, _react) {
-  'use strict';
+import _classCallCheck from 'babel-runtime/helpers/classCallCheck';
+import _possibleConstructorReturn from 'babel-runtime/helpers/possibleConstructorReturn';
+import _inherits from 'babel-runtime/helpers/inherits';
+import React from 'react';
 
-  Object.defineProperty(exports, "__esModule", {
-    value: true
-  });
+var ScrollToTop = function (_React$Component) {
+  _inherits(ScrollToTop, _React$Component);
 
-  var _getPrototypeOf2 = _interopRequireDefault(_getPrototypeOf);
+  function ScrollToTop() {
+    _classCallCheck(this, ScrollToTop);
 
-  var _classCallCheck3 = _interopRequireDefault(_classCallCheck2);
-
-  var _createClass3 = _interopRequireDefault(_createClass2);
-
-  var _possibleConstructorReturn3 = _interopRequireDefault(_possibleConstructorReturn2);
-
-  var _inherits3 = _interopRequireDefault(_inherits2);
-
-  var _react2 = _interopRequireDefault(_react);
-
-  function _interopRequireDefault(obj) {
-    return obj && obj.__esModule ? obj : {
-      default: obj
-    };
+    return _possibleConstructorReturn(this, _React$Component.apply(this, arguments));
   }
 
-  var ScrollToTop = function (_React$Component) {
-    (0, _inherits3.default)(ScrollToTop, _React$Component);
+  ScrollToTop.prototype.componentDidMount = function componentDidMount(prevProps) {
+    window.scrollTo(0, 0);
+  };
 
-    function ScrollToTop() {
-      (0, _classCallCheck3.default)(this, ScrollToTop);
-      return (0, _possibleConstructorReturn3.default)(this, (ScrollToTop.__proto__ || (0, _getPrototypeOf2.default)(ScrollToTop)).apply(this, arguments));
-    }
+  ScrollToTop.prototype.componentDidUpdate = function componentDidUpdate(prevProps) {
+    if (this.props.location !== prevProps.location) window.scrollTo(0, 0);
+  };
 
-    (0, _createClass3.default)(ScrollToTop, [{
-      key: 'componentDidMount',
-      value: function componentDidMount(prevProps) {
-        window.scrollTo(0, 0);
-      }
-    }, {
-      key: 'componentDidUpdate',
-      value: function componentDidUpdate(prevProps) {
-        if (this.props.location !== prevProps.location) window.scrollTo(0, 0);
-      }
-    }, {
-      key: 'render',
-      value: function render() {
-        return this.props.children;
-      }
-    }]);
-    return ScrollToTop;
-  }(_react2.default.Component);
+  ScrollToTop.prototype.render = function render() {
+    return this.props.children;
+  };
 
-  exports.default = ScrollToTop;
-});
+  return ScrollToTop;
+}(React.Component);
+
+export default ScrollToTop;
